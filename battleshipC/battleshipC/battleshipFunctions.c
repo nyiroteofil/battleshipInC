89bdc64809254a3c5d531779b8d465d037b7df4b
nyiroteofil <103335721+nyiroteofil@users.noreply.github.com>
2023-12-02 15:45:35 +0100

    PlayerInitialization completed
    PlayerInitialization() probably has som errors

--- battleshipC/battleshipC/battleshipFunctions.c
+++ battleshipC/battleshipC/battleshipFunctions.c
@@ -4,7 +4,7 @@
 
 #include "dataStructs.c"
 
-struct Player InitializePlayer(int numP, /*n-th player*/int numD /*dimensions of the playing field*/, int numS /*dimensions of ships*/) {
+struct Player InitializePlayer(int numP, /*n-th player*/int numD /*dimensions of the playing field*/, int numS /*length of the largest ship*/) {
 
 	struct Player playerInstance;
 	playerInstance.playerTable = (char**)malloc(sizeof(char) * (numD * numD));
@@ -16,44 +16,81 @@ struct Player InitializePlayer(int numP, /*n-th player*/int numD /*dimensions of
 	}
 
 
-	struct Ship* ships = (struct Ship*)malloc(sizeof(struct Ship) * numP); //allocating memory for the ships
+	playerInstance.ships = (struct Ship*)malloc(sizeof(struct Ship) * numP); //allocating memory for the ships
 	
-	
+	for (int i = numS; i > 0; i--) {
+		struct Ship shipInstance;
 
+		int shipCollison = 0;
+		do
+		{
+			shipInstance.occupiedFields = getShipFields(numD, i);
 
-	
+			for (int si = 0; si < abs(i - numS); si++) {		// si = ship index
+				for (int fi = 0; fi < playerInstance.ships[si].size; fi++) {
+					for (int nf = 0; nf < i; nf++) {
+						int usedX = playerInstance.ships[si].occupiedFields[fi].x;
+						int usedY = playerInstance.ships[si].occupiedFields[fi].y;
+						int newX = shipInstance.occupiedFields[nf].x;
+						int newY = shipInstance.occupiedFields[nf].y;
+						
+						if (usedX == newX && usedY == newY) shipCollison = 1;
+					}
+				}
+			}
+
+		} while (shipCollison != 0);
+
+		playerInstance.ships[abs(i - numS)] = shipInstance;
+	}
+
 
 }
+
 
 struct Field* getShipFields( int numD, int shipSize) {
 
 	struct Field* fields = (struct Field*)malloc(sizeof(struct Field) * shipSize);
 
-	for (int i = 0; i > shipSize; i++) {
-		struct Field fieldInstance;
-		int orientation;
+	printf("Placing %d long ship:\n", shipSize);
+	int x, y;
+	int orientation;
+	int collison = 0;
 
+	do
+	{
 		do
 		{
-			do
-			{
-				printf("Ship's orientation (1: vertical | 2: horizontal): ");
-				scanf_s("%d", &orientation);
-			} while (orientation != 1 && orientation != 2);
+			printf("\tShip's orientation (1: vertical | 2: horizontal): ");
+			scanf_s("\t%d", &orientation);
+		} while (orientation != 1 && orientation != 2);
 
-			printf("Coordinate X: ");
-			scanf_s("%d", &fieldInstance.x);
-			printf("Coordinate Y: ");
-			scanf_s("%d", &fieldInstance.y);
+		printf("\tStarting coordinate X: ");
+		scanf_s("%d", &x);
+		printf("\tStarting coordinate Y: ");
+		scanf_s("%d", &y);
 
-			fieldInstance.x -= 1;
-			fieldInstance.y -= 1;
-			fieldInstance.state = 1;
+	} while (orientation != 2 || x + 8 > numD || x < 0 || y > numD || y < 0 &&
+		orientation != 1 || y + 8 > numD || y < 0 || x > numD || x < 0 && collison == 1);
 
-		} while (orientation != 2 || fieldInstance.x + 8 > numD || fieldInstance.x < 0 || fieldInstance.y > numD || fieldInstance.y < 0 &&
-				 orientation != 1 || fieldInstance.y + 8 > numD || fieldInstance.y < 0 || fieldInstance.x > numD || fieldInstance.x < 0);
-		
-		fields[i] = fieldInstance;
+	if (orientation == 1) {
+		for (int i = 0; i < shipSize; i++) {
+			struct Field fieldInstance;
+			fieldInstance.x = x;
+			fieldInstance.y = y + i;
+			fields[i] = fieldInstance;
+		}
 	}
+	else {
+		for (int i = 0; i < shipSize; i++) {
+			struct Field fieldInstance;
+			fieldInstance.x = x + i;
+			fieldInstance.y = y;
+			fields[i] = fieldInstance;
+		}
+	}
+	
 
+
+	return fields;
 }
