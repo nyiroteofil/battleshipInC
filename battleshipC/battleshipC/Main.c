b7c6c145df0cffb0d2514ac99aa9a84008faea3b
nyiroteofil <103335721+nyiroteofil@users.noreply.github.com>
2023-12-09 22:42:57 +0100

    Repaired the game after accidentaly damaged it with git

--- battleshipC/battleshipC/Main.c
+++ battleshipC/battleshipC/Main.c
@@ -4,7 +4,7 @@ typedef struct Player Player;
 typedef struct Ship Ship;
 typedef struct Field Field;
 
-#include "playerInit.h"
+#include "playerInit.h""
 #include "dataStructures.H"
 #include "utilityFunctions.h"
 #include "gameRound.h"
@@ -14,12 +14,12 @@ int main() {
 	int numberOfShips, gameDimensions;
 
 	do {
-		printf("Playing field size (N x N): ");
-		scanf_s("%d", &gameDimensions);
+		printf("Playing field size(N x N): ");
+		scanf_s("% d", &gameDimensions);
 		printf("Max size of the ships: ");
-		scanf_s("%d", &gameDimensions);
-		system("clr");
-	} while (gameDimensions < 5 || gameDimensions > 99 || numberOfShips > (int)((float)gameDimensions * (5/3)));
+		scanf_s("% d", &numberOfShips);
+		system("cls");
+	} while (gameDimensions > 99 || gameDimensions > 15 || numberOfShips > (int)((double)gameDimensions * 0.7));
 
 	Player players[2];
 	Player player1, player2;
@@ -27,11 +27,33 @@ int main() {
 	InitializePlayers(&player2, gameDimensions, numberOfShips);
 	players[0] = player1;
 	players[1] = player2;
-	
+
+	int gameState = 1;
 	do {
 		gameRound(&players[0], &players[1], gameDimensions);
-		switchPlayers(players);
-	} while (); // vége függvény megírása
+
+		if (isGameOver(&players[1]) || isGameOver(&players[1])) {
+			gameState = 0;
+		}
+		else {
+			switchPlayers(players);
+		}
+	} while (gameState);
+
+
+	printf("%s WON!!!", players[0].name);
+
+	//freeing the allocated memory
+	freeCharMatrix(players[0].hitMap, gameDimensions, gameDimensions);
+	freeCharMatrix(players[0].playerTable, gameDimensions, gameDimensions);
+	freeCharMatrix(players[1].hitMap, gameDimensions, gameDimensions);
+	freeCharMatrix(players[1].playerTable, gameDimensions, gameDimensions);
+	for (int i = 0; i < numberOfShips; i++) {
+		free(players[0].ships[i].occupiedFields);
+		free(players[1].ships[i].occupiedFields);
+	}
+	free(players[0].ships);
+	free(players[1].ships);
 
 	return 0;
 }
--- battleshipC/battleshipC/playerInit.h
+++ battleshipC/battleshipC/playerInit.h
@@ -16,58 +16,43 @@ typedef struct Player Player;
 typedef struct Ship Ship;
 typedef struct Field Field;
 
-char** AllocateCharMatrix(int sizeX, int sizeY) {
-	char** matrix = (char**)malloc(sizeof(char*) * sizeY);
-	for (int i = 0; i < sizeX; i++) {
-		matrix[i] = (char*)malloc(sizeof(char) * sizeX);
-	}
-
-	return matrix;
-}
-
 int isShipValid(int x, int y, int ori, int size, int dim) {
 	size -= 1;
 	if (ori == 2) {
-		if (y + size < dim && y > -1 && x <= dim && x > -1) return 1;
+		if (y + size < dim && y > -1 && x < dim && x > -1) return 1;
 	}
 	else if (ori == 1) {
-		if (y <= dim && y > -1 && x - size < dim && x > -1) return 1;
+		if (y < dim && y > -1 && x - size > -1 && x < dim) return 1;
 	}
 
-	printf("!!! Invalid ship placement: Invalid Coordinates !!!\n\n");
+	printf("\n\n!!! Invalid ship placement: Invalid Coordinates !!!\n\n");
 	return 0;
 }
 
 
-Field* getShipsFields(int starterX, int starterY, int size, int orientation) {
-	Field* fields = (Field*)malloc(sizeof(Field) * size);
+Field getShipsFields(int offset, int starterX, int starterY, int orientation) {
+	Field shipField;
 	if (orientation == 1) {
-		for (int i = 0; i < size; i++) {
-			Field fieldInstance;
-			fieldInstance.x = starterX - i;
-			fieldInstance.y = starterY;
-			fieldInstance.state = 1;
-			fields[i] = fieldInstance;
-		}
-
-		return fields;
+		shipField.x = starterX - offset;
+		shipField.y = starterY;
+		shipField.state = 1;
 	}
 	else if (orientation == 2) {
-		for (int i = 0; i < size; i++) {
-			Field fieldInstance;
-			fieldInstance.x = starterX;
-			fieldInstance.y = starterY + i;
-			fields[i] = fieldInstance;
-		}
-
-		return fields;
+		shipField.x = starterX;
+		shipField.y = starterY + offset;
+		shipField.state = 1;
 	}
 
-	return fields;
+	return shipField;
 }
 
 int willShipCollide(int x, int y, int size, int ori, Ship* ships) {
-	Field* shipsFields = getShipsFields(x, y, size, ori);
+	Field* shipsFields = (Field*)malloc(sizeof(Field) * size);
+
+	for (int i = 0; i < size; i++) {
+		shipsFields[i] = getShipsFields(i, x, y, ori);
+	}
+
 	for (int i = 0; i < size - 1; i++) {
 		for (int j = 0; j < size - 1; j++) {
 
@@ -79,7 +64,7 @@ int willShipCollide(int x, int y, int size, int ori, Ship* ships) {
 			}
 
 			if (k < size) {
-				printf("!!! Invalid ship placement: Ship Collison !!!\n\n");
+				printf("\n\n!!! Invalid ship placement: Ship Collison Iminent !!!\n\n");
 				return 1;
 			}
 
@@ -90,15 +75,10 @@ int willShipCollide(int x, int y, int size, int ori, Ship* ships) {
 }
 
 
-void getShips(int numS, int dim, Player* player) {
-	player->ships = (Ship*)malloc(sizeof(Ship) * numS);
-
-	for (int i = 1; i <= numS; i++) {
-
+void getShips(int dim, Player* player) {
+	player->ships = (Ship*)malloc(sizeof(Ship) * player->numberOfShips);
+	for (int i = 1; i <= player->numberOfShips; i++) {
 		system("cls");
-
-		Ship shipInstance;
-
 		int x, y, orientation;
 
 		renderMap(player->playerTable, dim);
@@ -106,18 +86,16 @@ void getShips(int numS, int dim, Player* player) {
 		do {
 
 			do {
-				
+
 				printf("PLACEING %d LONG SHIP\n", i);
 				do {
 					printf("\tOrientation of the ships (1: vertical | 2: horizontal): ");
 					scanf_s("%d", &orientation);
 				} while (orientation != 1 && orientation != 2);
-
 				printf("\tX starting coordinate: ");
-				scanf_s("%d", &x);
+				scanf("%d", &x);
 				printf("\tY starting coordinate: ");
-				scanf_s("%d", &y);
-				printf("\n");
+				scanf("%d", &y);
 
 				x -= 1;
 				y -= 1;
@@ -127,26 +105,46 @@ void getShips(int numS, int dim, Player* player) {
 
 		} while (!isShipValid(x, y, orientation, i, dim));
 
-		shipInstance.size = i;
-		shipInstance.occupiedFields = getShipsFields(x, y, i, orientation);
-		shipInstance.state = 1;
-		player->ships[i - 1] = shipInstance;
+		player->ships[i - 1].occupiedFields = (Field*)malloc(sizeof(Field) * i);
 
-		updatePlayerTable(player);
+		for (int j = 0; j < i; j++) {
+			player->ships[i - 1].occupiedFields[j] = getShipsFields(j, x, y, orientation);
+		}
+
+		player->ships[i - 1].size = i;
+		player->ships[i - 1].state = 1;
+
+		updateIncompleteMap(player, i);
 	}
 }
 
-void InitializePlayers(Player* player1, int dimension, int numOfShips) {
-	player1->playerTable = AllocateCharMatrix(dimension, dimension);
-	player1->hitMap = AllocateCharMatrix(dimension, dimension);
+void InitializePlayers(Player* player1, int dimension, int numberOfShips) {
+	system("cls");
+	printf("Player's Name: ");
+	scanf("%s", player1->name);
+	system("cls");
+
+	player1->numberOfShips = numberOfShips;
+
+
+	player1->playerTable = (char**)malloc(sizeof(char*) * dimension);
+	for (int i = 0; i < dimension; i++) {
+		player1->playerTable[i] = (char*)malloc(sizeof(char) * dimension);
+	}
+
+	player1->hitMap = (char**)malloc(sizeof(char*) * dimension);
+	for (int i = 0; i < dimension; i++) {
+		player1->hitMap[i] = (char*)malloc(sizeof(char) * dimension);
+	}
 
 	for (int i = 0; i < dimension; i++) {
 		for (int j = 0; j < dimension; j++) {
 			player1->playerTable[i][j] = '*';
-			player1->hitMap[i][j] = '?';
+			player1->hitMap[i][j] = '*';
 		}
 	}
 
-	getShips(numOfShips, dimension, player1);
+	getShips(dimension, player1);
 }
+
 #endif
